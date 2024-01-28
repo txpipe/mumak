732d9aa937390fde7b9a90c535f602849d85e80a
Santiago Carmuega <santiago@carmuega.me>
2024-01-28 10:03:05 -0300

    chore: add sample functions for testing

--- extension/src/lib.rs
+++ extension/src/lib.rs
@@ -1,4 +1,7 @@
-use pallas::ledger::traverse::MultiEraBlock;
+use pallas::ledger::{
+    addresses::Address,
+    traverse::{MultiEraBlock, MultiEraTx},
+};
 use pgrx::prelude::*;
 
 pgrx::pg_module_magic!();
@@ -12,6 +15,40 @@ fn hello_extension() -> &'static str {
 fn block_hash(bytes: &[u8]) -> String {
     let block = MultiEraBlock::decode(bytes).unwrap();
     block.hash().to_string()
+}
+
+#[pg_extern]
+fn tx_has_address(tx_cbor: &[u8], tx_era: u32, address: &[u8]) -> bool {
+    let expected = match Address::from_bytes(address) {
+        Ok(x) => x,
+        Err(_) => return false,
+    };
+
+    let tx = match MultiEraTx::decode(tx_cbor) {
+        Ok(x) => x,
+        Err(_) => return false,
+    };
+
+    tx.outputs()
+        .iter()
+        .map(|x| x.address().ok())
+        .flatten()
+        .any(|x| x == expected)
+}
+
+#[pg_extern]
+fn tx_output_addresses(tx_cbor: &[u8], tx_era: u32) -> Vec<Vec<u8>> {
+    let tx = match MultiEraTx::decode(tx_cbor) {
+        Ok(x) => x,
+        Err(_) => return vec![],
+    };
+
+    tx.outputs()
+        .iter()
+        .map(|x| x.address().ok())
+        .flatten()
+        .map(|x| x.to_vec())
+        .collect()
 }
 
 #[cfg(any(test, feature = "pg_test"))]
